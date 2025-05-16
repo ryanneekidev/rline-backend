e36b1b4b9adc4cf6c7b1a646eb0d48fad28c97c7
Rayan Mamhoud <kiraclover@protonmail.com>
2025-05-16 01:00:34 +0100

    yes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
     origin: "https://frontend-test-one-coral.vercel.app",
-    methods: ['GET']
+    methods: 'GET'
 }))
 
 require('dotenv').config()
