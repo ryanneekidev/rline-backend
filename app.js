14c42d568577a388c986a7e7405f5eb0be155165
Mamhoud Rayan <kiraclover@protonmail.com>
2025-12-01 14:33:32 +0800

    s

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
     origin: "https://rline.ryanneeki.xyz",
+    credentials: true,
     methods: 'GET,POST,OPTIONS'
 }))
 
